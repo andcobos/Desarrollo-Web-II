8e2d89fca2236051fd0d68b2a6f206e9a1984898
andcobos <146792784+andcobos@users.noreply.github.com>
2025-03-21 09:39:31 -0600

    creacion guia 6

---
+++ Guia6_202351111/practica6/src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import router from './routes';
+import { connectBD } from './config/db';
+
+const PORT = 3000;
+const app = express();
+
+app.use(express.json()); // Middleware para procesar JSON en las solicitudes
+app.use('/', router); // Configuración de rutas
+
+connectBD().then(() => {
+    console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+
+export default app;
---
+++ Guia6_202351111/practica6/src/routes.ts
@@ -0,0 +1,14 @@
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+router.get('/', (req: Request, res: Response) => {
+  res.status(200).send('Bienvenido a la API 🚀');
+});
+
+router.post('/auth/register', (req, res) => {
+  console.log(req.body);
+  res.status(201).json({ message: 'Datos del usuario recibidos con éxito' });
+});
+
+export default router;
