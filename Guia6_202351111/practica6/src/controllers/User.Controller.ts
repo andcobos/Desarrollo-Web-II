0a36605cf1736c478ec55913de41a2cfa0c9c0c5
andcobos <146792784+andcobos@users.noreply.github.com>
2025-03-23 19:12:02 -0600

    update

--- Guia6_202351111/practica6/src/controllers/User.Controller.ts
+++ Guia6_202351111/practica6/src/controllers/User.Controller.ts
@@ -2,7 +2,21 @@ import { Request, Response } from "express";
 import User from "../models/Users";
 
 export const createAccount = async (req: Request, res: Response) => {
+    const { name, password, email, username } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        res.status(409).json({ message: "User already exists for this email" });
+        return;
+    }
+
+    const usernameExists = await User.findOne({ username });
+    if (usernameExists) {
+        res.status(409).json({ message: "Username already exists" });
+        return;
+    }
+
     const user = new User(req.body);
     await user.save();
-    res.status(201).json({ message: 'Datos del usuario recibidos con éxito' });
-};
+    res.status(201).json({ message: 'User created successfully' });
+}
