9abe3555c88fec8b2d60569cda7cc23a7e2985a4
andcobos <146792784+andcobos@users.noreply.github.com>
2025-03-23 20:47:43 -0600

    finish guia 6

--- Guia6_202351111/practica6/src/controllers/User.Controller.ts
+++ Guia6_202351111/practica6/src/controllers/User.Controller.ts
@@ -1,7 +1,15 @@
 import { Request, Response } from "express";
 import User from "../models/Users";
+import { hashPassword } from "../utils/auth";
+import { validationResult } from 'express-validator';
 
 export const createAccount = async (req: Request, res: Response) => {
+    let errors = validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors:errors.array() });
+    }
+
     const { name, password, email, username } = req.body;
 
     const userExists = await User.findOne({ email });
@@ -17,6 +25,7 @@ export const createAccount = async (req: Request, res: Response) => {
     }
 
     const user = new User(req.body);
+    user.password = await hashPassword(password);
     await user.save();
     res.status(201).json({ message: 'User created successfully' });
 }
--- Guia6_202351111/practica6/src/routes.ts
+++ Guia6_202351111/practica6/src/routes.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from 'express';
 import User from './models/Users';
 import { createAccount } from './controllers/User.Controller';
+import { body } from 'express-validator';
 
 const router = Router();
 
@@ -8,13 +9,15 @@ router.get('/', (req: Request, res: Response) => {
   res.status(200).send('Bienvenido a la API 🚀');
 });
 
-router.post('/auth/register', async(req, res) => {
-  console.log(req.body);
-  const user= new User(req.body);
-  await user.save();
-  res.status(201).json({ message: 'Datos del usuario recibidos con éxito' });
+router.post('/auth/register', [
+  body("name").isString().notEmpty().withMessage("Name is required"),
+  body("password").isString().isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters"),
+  body("email").isEmail().withMessage("Invalid email"),
+  body("username").isString().notEmpty().withMessage("Username is required")], 
+  async (req, res) => {
+    createAccount(req, res);
 });
 
-router.post('/auth/register', createAccount);
 
 export default router;
